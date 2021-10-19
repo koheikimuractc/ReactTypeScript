e71b16c97c938c4ab515cb85971ade0d5ceba25a
koheikimuractc <92065874+koheikimuractc@users.noreply.github.com>
2021-10-19 19:30:14 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import "./styles.css";
+import { useState } from "react";
+import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "kimura",
+  hobbies: ["Game", "golf"]
+};
+
+export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
+  return (
+    <div className="App">
+      <UserProfile user={user} />
+      <Text color={"red"} fontSize={"12"}>
+        aaaaa
+      </Text>
+      <button onClick={onClickFetchData}>GET DATA</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
+    </div>
+  );
+}
